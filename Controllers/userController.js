45e1b71de6ecff5fa53eb644bf46a6a29215342a
katemba fahad <katsfahad008@gmail.com>
2024-10-04 10:32:25 +0300

    Setup get api for all users

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -2,6 +2,13 @@ const {PrismaClient} = require('@prisma/client')
 const prisma  = new PrismaClient()
 
 const getAllUsers = (req,res) =>{
+    const users = prisma.user.findMany()
+
+    if(users){
+        res.json(users)
+    }else{
+        res.json('No Users found')
+    }
 }
 
 module.exports = {
