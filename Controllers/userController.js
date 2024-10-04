263ea00c3cb202e312b82e7577536b13cb1cf45a
katemba fahad <katsfahad008@gmail.com>
2024-10-04 11:31:22 +0300

    Work on get api

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,32 +1,31 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma  = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const getAllUsers = async (req,res) =>{
-    const users = await prisma.user.findMany()
+const getAllUsers = async (req, res) => {
+  const users = await prisma.user.findMany();
+  if (users) {
+    res.json(users);
+  } else {
+    res.json("No Users found");
+  }
+};
 
-    if(users){
-        res.json(users)
-    }else{
-        res.json('No Users found')
-    }
-}
-
-const createUser = async (req,res) =>{
-    const {name, email} = req.body
-    if(name && email){
-        const newUser = await prisma.user.create({
-            data:{
-                name,
-                email
-            }
-        })
-        res.json('New user created')
-    }else{
-        res.json('Add name or email')
-    }
-}
+const createUser = async (req, res) => {
+  const { name, email } = req.body;
+  if (name && email) {
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    res.json("New user created");
+  } else {
+    res.json("Add name or email");
+  }
+};
 
 module.exports = {
-    getAllUsers,
-    createUser
-}
+  getAllUsers,
+  createUser,
+};
